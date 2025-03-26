eb38232bf8c349f43783d488937f8d08a8407ac6
Ishit Arhatia <104547837+ia4226@users.noreply.github.com>
2025-03-26 12:32:53 +0530

    Delete major_proj directory

--- major_proj/Wander_Vision_Major/utils.py
+++
@@ -1,9 +0,0 @@
-# utils.py
-import re
-
-def clean_direction_text(direction):
-    clean_text = re.sub(r"<.*?>", "", direction)
-    clean_text = re.sub(r"Pass by.*?(?=\(|$)", "", clean_text).strip()
-    return clean_text
-
-
